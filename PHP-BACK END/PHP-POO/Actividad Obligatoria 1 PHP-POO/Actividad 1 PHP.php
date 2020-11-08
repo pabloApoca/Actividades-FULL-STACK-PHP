10f9dbd57d9762b3bf1bd8d0cfdaf801c9f90a23
Pablo Lopez <pablo_mtv08@hotmail.com>
2020-11-08 17:15:33 -0300

    Ordene las carpetas por BACK-END Y FRONT-END y agregue la primera actividad obligatoria de PHP/POO en el BACK-END

---
+++ PHP-BACK END/PHP-POO/Actividad Obligatoria 1 PHP-POO/Actividad 1 PHP.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<html lang="es">
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>Primera página PHP</title>
+
+</head>
+
+<body>
+
+<br>
+<br>
+<br>
+
+<?php
+
+echo "<div align='center'><u>Hola Mundo!! Soy Pablo Martin Lopez. </u></div><br>";
+
+?>
+
+<br>
+<br>
+<br>
+
+</body>
+
+</html>
