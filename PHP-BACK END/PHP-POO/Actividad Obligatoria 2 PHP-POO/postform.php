e40b69da7dee075c6eb78067ba19b08ed09dacfb
Pablo Martin Lopez <pablo_mtv08@hotmail.com>
2020-11-20 00:02:44 +0100

    Agregue los link de la actividad obligatoria numero 2 de PHP-BACK-END

--- PHP-BACK END/PHP-POO/Actividad Obligatoria 2 PHP-POO/postform.php
+++ PHP-BACK END/PHP-POO/Actividad Obligatoria 2 PHP-POO/postform.php
@@ -11,7 +11,8 @@ $clave=$_POST['clave'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  
+    <!-- Link de la actividad http://www.cursophp.com.ar/38277934/postform.php-->
+
     <style>
         body{ 
         margin: 0;
